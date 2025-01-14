38325e382e21edaf01eade059cd097a8349e7658
Vogon-P0et <paul@innvisia.com>
2025-01-14 18:33:50 -0500

    completed assignment

---
+++ README.md
@@ -0,0 +1,32 @@
+# Starting Your Codewars Journey
+
+### Paul Jaghab
+### January 2025 Cohort
+### Module 1 Assignment 1 <br><br>
+  
+#### The aim of the assignment was to train on our first CodeWars katas. We were given three simple katas to complete. Please see the python code in this repository for more comments on the assignment.
+
+In the assignment file, you will see that the functions have been created for you with return statements. If you would like to test various arguments when calling the function, please change the value of the "test" variable for each problem to reflect the desired input.
+
+#### Below you will find the instructions directly from CodeWars for the assignment:
+
+
+##### Problem 1 Description:
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+##### Problem 2 Description:
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+
+##### Problem 3 Description
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
--- codewars_journey.py
+++ codewars_journey.py
@@ -1,19 +1,35 @@
-# # codewars - even or odd
-# def even_or_odd(number):
-#   if int(number) % 2 == 0:
-#     return "Even"
-#   else:
-#     return "Odd"
+# codewars | even or odd
+# Description:
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+def even_or_odd(number):
+  if int(number) % 2 == 0:
+    return "Even"
+  else:
+    return "Odd"
+
+test_num = 34
+print(even_or_odd(test_num))
 
 
-# # codewars - Numbers to strings
-# def number_to_string(num):
-#     return str(num)
+# codewars | Numbers to strings
+# the simplest answer to the "numbers to strings" problem seems to be type casting to a string
+def number_to_string(num):
+    return str(num)
+
+test_str_to_num = "78908765456789"
+print(type(number_to_string(test_str_to_num)))
 
 
-def vowel_count(inputStr):
-    inputStr.count("a", "e", "i", "o", "u")
-    if inputStr == True:
-        inputStr.count("a", "e", "i")
-        inputStr.count("o", "u")
-        print(inputStr.count("a", "e", "i") + inputStr.count("o", "u"))
+# codewars | vowel count
+# this was actually harder than expected. I tried a few times to get an answer without using a list.
+# I think a list comprehension would also have worked but I actually understand loops better than list comprehensions at this moment
+def vowel_count(Str):
+    vowels = ["a", "e", "i", "o", "u"]
+    count = 0
+    for char in Str:
+        if char in vowels:
+            count += 1
+    return(count)
+
+test_vowel_count = "i want to count the number of values in a string"
+print(vowel_count(test_vowel_count))
